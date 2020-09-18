d1795b1f14a53590027d1ffc70c6841b7b574cae
jsthk69 <40279238+jsthk69@users.noreply.github.com>
2020-09-18 17:34:44 +0300

    Add files via upload

---
+++ laba6/script.js
@@ -0,0 +1,50 @@
+﻿function resh() {
+	var x = parseFloat(document.getElementById("x").value);
+	var y = parseFloat(document.getElementById("y").value);
+	
+	//Проверка на пустую строку или символ не являющийся числом
+	if(isNaN(x) == true) x = 0;
+	if(isNaN(y) == true) y = 0;
+	
+	var f;
+	if (document.getElementById('f1').checked == true) { f = parseInt(document.getElementById('f1').value); }
+	if (document.getElementById('f2').checked == true) { f = parseInt(document.getElementById('f2').value); }
+	if (document.getElementById('f3').checked == true) { f = parseInt(document.getElementById('f3').value); }
+
+	var c;
+	var z;
+	var v;
+		switch (f) { 
+			case 1:
+				c = Math.cos(y);
+				z = Math.cos(Math.tan(y));
+				v = Math.cos(x);
+				break;
+			case 2:
+				c = Math.sqrt(y);
+				z = Math.sqrt(Math.tan(y));
+				v = Math.sqrt(x);
+				break;
+			case 3:
+				c = Math.exp(y);
+				z = Math.exp(Math.tan(y));
+				v = Math.exp(x);
+				break;
+        }
+		
+	var m;
+           if ((x / y) > 0) {
+			   m = (1 / Math.tan(x)) + c;
+		   }
+		   if ((x / y) < 0) {
+			   m = Math.log(x) + z;
+		   }
+		   else {
+			   m = v * Math.pow(y, 3);
+		   }
+		 
+	if (document.getElementById('round').checked)
+            { m = Math.round(m); }
+
+	document.getElementById("result").innerHTML = m;
+}
---
+++ laba7/script.js
@@ -0,0 +1,10 @@
+﻿function resh() {
+	let a = parseFloat(document.getElementById("a").value);
+	let b = parseFloat(document.getElementById("b").value);
+	let c = parseFloat(document.getElementById("c").value);
+
+	let min;
+	(a < b) && (a < c) ? min = a : ((b < a) && (b < c) ? min = b: min = c);
+	
+	document.getElementById("result").innerHTML = min;
+}
